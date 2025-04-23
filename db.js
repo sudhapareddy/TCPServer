81e351351534732a3c113f6b0ad5f758ad18f3cb
sudhakarareddy <sudhapareddy@gmail.com>
2025-04-23 12:52:01 +0530

    dbfile

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 
 const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(mongoURI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
