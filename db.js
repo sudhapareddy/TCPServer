6fcacc3ab59f37f24856cfc2ddbb3fe703e6a9d8
sudhakarareddy <sudhapareddy@gmail.com>
2025-04-23 14:29:35 +0530

    dbfile

--- db.js
+++ db.js
@@ -10,10 +10,11 @@ const mongoose = require("mongoose");
 //require("dotenv").config();
 
 //const mongo_URI = process.env.MONGO_URI;
+console.log("MONGO_URI:", process.env.MONGO_URI); // ✅ this works
 
-console.log("MONGO_URI:", MONGO_URI);
+//console.log("MONGO_URI:", MONGO_URI);
 
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
